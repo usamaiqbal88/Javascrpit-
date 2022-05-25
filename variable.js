a42bbdc5f3380ad7c4c16d283d787bdce8d9a748
Usama Iqbal <usamaiqbal532@gmail.com>
2022-05-26 00:20:10 +0500

    variable practine done

---
+++ variable.js
@@ -0,0 +1,18 @@
+//Variables are containers for storing data (storing data values).
+//Always declare JavaScript variables with var,let, orconst.
+//If you want a general rule: always declare variables with const.
+//If you think the value of the variable can change, use let.
+
+var a=5;
+var b=10;
+var c= a+b;
+console.log(c);
+
+console.log(a + b + c);
+console.log("5"+ b + c);
+
+console.log("sam" + a);
+console.log("Usama" + " " + "Iqbal");
+
+let _a=20, _1=100; $=50;  //declare in one line, separated by coma.
+console.log(_a + _1 +$);
