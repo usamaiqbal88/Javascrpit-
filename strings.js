6629ff36a31080d21fea1ce9410d7fc406a455a0
Usama Iqbal <usamaiqbal532@gmail.com>
2022-05-28 23:56:32 +0500

    String use done.

---
+++ strings.js
@@ -0,0 +1,15 @@
+console.log("Strings...");
+//JavaScript strings are for storing and manipulating text.
+let name="sam";
+let dream="Become a 'IT Expert'.";  //we can use quotes inside a string such that they don't match the quotes surrounding the string.
+console.log(name+"\n"+dream);
+
+let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let length = text.length;           // to find the length of string.
+console.log(length);
+
+// Another solution to use single/double quote in single/double quote.
+let address = 'New Street \'sipher\' , California.';  //The sequence \'  inserts a double quote in a string.
+let address2 = "New Street \"sipher\" , California.";  //The sequence \"  inserts a double quote in a string.
+console.log(address+"\n"+address2);
+
